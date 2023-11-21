6b2501201141a239be0d637c0dfbce59b331161d
Ankcorn <thomasankcorn@gmail.com>
2023-11-21 11:16:54 +0000

    try js instrumentation

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import * as trace from 'next/dist/trace';
 type Data = {
   name: string
 }
-
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  const span = trace.trace('api');
+  span.stop()
   res.status(200).json({ name: 'John Doe' })
 }
